3422baa9020b4f952ec9e92edb0062a092950c4e
laraoyku <l.oyku2003@gmail.com>
2025-12-18 20:13:27 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.quizmaster {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.quizmaster to javafx.fxml;
-    exports com.example.quizmaster;
-}
